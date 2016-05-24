a2f8be8418cd0d8f4d787dc4ce1bc6bb1d10200c
marinacor1 <marinaspam1234@gmail.com>
2016-05-24 10:36:44 -0600

    Refactor with open struct and remove initialize method

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,16 +4,9 @@ class Artist < OpenStruct
     ChaimzService.new
   end
 
-  def initialize(new_artist_hash)
-    @id = new_artist_hash[:id]
-    @name = new_artist_hash[:name]
-    @created_at = new_artist_hash[:created_at]
-    @updated_at = new_artist_hash[:updated_at]
-  end
-
   def self.find(id)
-    temp_artist_hash = service.artist_hash(id)
-    Artist.new(temp_artist_hash)
+    artist_hash = service.artist_hash(id)
+    Artist.new(artist_hash)
   end
 
   def self.all
