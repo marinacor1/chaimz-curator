26802a0695fd7030838574742e954be65050d90a
marinacor1 <marinaspam1234@gmail.com>
2016-05-24 10:29:45 -0600

    Refactor service within model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,10 @@ class Artist
 
   attr_accessor :id, :name, :created_at, :updated_at
 
+  def self.service
+    ChaimzService.new
+  end
+
   def initialize(new_artist_hash)
     @id = new_artist_hash[:id]
     @name = new_artist_hash[:name]
@@ -10,12 +14,12 @@ class Artist
   end
 
   def self.find(id)
-    temp_artist_hash = ChaimzService.new.artist_hash(id)
+    temp_artist_hash = service.artist_hash(id)
     Artist.new(temp_artist_hash)
   end
 
   def self.all
-    artist_array = ChaimzService.new.artists_hash
+    artist_array = service.artists_hash
     artist_array.map do |artist_hash|
       Artist.new(artist_hash)
     end
