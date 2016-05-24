571b9480b13cd4828aa5f8a05bb9a6221648a60d
marinacor1 <marinaspam1234@gmail.com>
2016-05-24 10:14:22 -0600

    initialize and attr accessors created within artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,11 @@
 class Artist
-  def initialize
+
+  attr_accessor :id, :name, :created_at, :updated_at
+  
+  def initialize(artist_hash)
+    @id = artist_hash[:id]
+    @name = artist_hash[:name]
+    @created_at = artist_hash[:created_at]
+    @updated_at = artist_hash[:updated_at]
   end
 end
