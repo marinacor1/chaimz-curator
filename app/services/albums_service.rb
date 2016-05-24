8787ac85e02ce7f7c9b0f4f8d8e93b26414f130e
marinacor1 <marinaspam1234@gmail.com>
2016-05-24 11:07:18 -0600

    Add albums service

---
+++ app/services/albums_service.rb
@@ -0,0 +1,23 @@
+class AlbumsService
+  def initialize
+    @connection = Faraday.new(url: "https://my-chaimz.herokuapp.com/api/v1/")
+    @connection.headers["Authorization"] = "Bearer 35148ad62db32ff044d6df2cd57"
+  end
+
+  def get_albums
+    @connection.get "albums"
+  end
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_song(album_id)
+    @connection.get "albums/album_id"
+  end
+
+  def album_hash
+    parse(get_albums)
+  end
+
+end
